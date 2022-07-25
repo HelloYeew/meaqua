971c0396f3e57af877a7043853cb153da53f2459
Phawit Pornwattanakul <me@helloyeew.dev>
2022-07-25 22:43:04 +0700

    Add essential element for video background

--- meaqua/settings.py
+++ meaqua/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
