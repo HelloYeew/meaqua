13cb7c29d649f2d1d29fb60831378e31f2f18626
Phawit Pornwattanakul <me@helloyeew.dev>
2022-07-27 09:47:37 +0700

    Fix template sometime in null state

--- theme/views.py
+++ theme/views.py
@@ -4,11 +4,11 @@ from users.models import HomeSettings
 
 def home(request):
     if request.user.is_authenticated:
-        theme = HomeSettings.objects.filter(user=request.user).first().current_theme
+        theme_user_setting = HomeSettings.objects.filter(user=request.user).first()
         # If theme is not set, use default theme
-        if theme is None:
+        if theme_user_setting is None:
             return render(request, 'index.html', {'use_default_theme': True})
         else:
-            return render(request, 'index.html', {'theme': theme})
+            return render(request, 'index.html', {'theme': theme_user_setting.current_theme})
     else:
         return render(request, 'index.html', {'use_default_theme': True})
