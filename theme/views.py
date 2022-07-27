5d7371fc0faa7c7fe2c20ae4cf6bcac458653ce8
Phawit Pornwattanakul <me@helloyeew.dev>
2022-07-28 03:17:57 +0700

    Add toast on login and modify setting

--- theme/views.py
+++ theme/views.py
@@ -3,12 +3,14 @@ from users.models import HomeSettings
 
 
 def home(request):
+    # Get login form from django.contrib.auth.forms
+
     if request.user.is_authenticated:
         theme = HomeSettings.objects.filter(user=request.user).first().current_theme
         # If theme is not set, use default theme
         if theme is None:
             return render(request, 'index.html', {'use_default_theme': True})
         else:
-            return render(request, 'index.html', {'theme': theme})
+            return render(request, 'index.html', {'use_default_theme': False, 'theme': theme})
     else:
         return render(request, 'index.html', {'use_default_theme': True})
