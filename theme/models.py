dfedd11da71e3a863af924ff513beecb3b6b482e
Phawit Pornwattanakul <me@helloyeew.dev>
2022-07-30 01:11:10 +0700

    Add welcome text to database

--- theme/models.py
+++ theme/models.py
@@ -21,8 +21,8 @@ class Theme(models.Model):
     color_primary = ColorField(default='#DFD9D6')
     color_accent = ColorField(default='#DBC2D1')
     color_background = ColorField(default='#0A0A0A')
-    # If video is deleted, the theme must still exist
     video = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True, blank=True)
+    welcome_text = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name + ' by ' + self.user.username
