a0bb8cc9b8e60b3d1ce5ecc3758a21ccdbd051bf
Phawit Pornwattanakul <me@helloyeew.dev>
2022-08-17 02:24:05 +0700

    Remove trailing slash

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/settings', views.control_panel_home, name='control_panel_home'),
-    path('/theme/gallery', views.theme_gallery, name='theme_gallery'),
+    path('settings', views.control_panel_home, name='control_panel_home'),
+    path('theme/gallery', views.theme_gallery, name='theme_gallery'),
 ]
