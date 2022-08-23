c30ec120f5284f12626cd8dc4a6d89850de78920
Phawit Pornwattanakul <me@helloyeew.dev>
2022-08-23 09:57:03 +0700

    Add manage applications page

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('settings', views.control_panel_home, name='control_panel_home'),
     path('settings/theme/gallery', views.theme_gallery, name='theme_gallery'),
     path('settings/theme/create', views.create_theme, name='create_theme'),
+    path('settings/applications/manage', views.manage_applications, name='manage_applications'),
 ]
--- control_panel/views.py
+++ control_panel/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from control_panel.forms import CreateNewThemeForm
-from theme.models import Theme
+from theme.models import Theme, Application
 from users.models import HomeSettings
 
 
@@ -58,4 +58,19 @@ def create_theme(request):
         'create_form': create_form,
         'current_theme': theme_user_setting.current_theme,
         'use_default_theme': use_default_theme
+    })
+
+
+@login_required
+def manage_applications(request):
+    theme_user_setting = HomeSettings.objects.filter(user=request.user).first()
+    # If theme is not set, use default theme
+    if theme_user_setting.current_theme is None:
+        use_default_theme = True
+    else:
+        use_default_theme = False
+    return render(request, 'control_panel/application/manage.html', {
+        'current_theme': theme_user_setting.current_theme,
+        'use_default_theme': use_default_theme,
+        'applications': Application.objects.filter(user=request.user)
     })
